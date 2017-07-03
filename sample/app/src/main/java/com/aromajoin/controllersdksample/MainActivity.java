4abb87318f93abba265bc1fd4964aeb12fc69ec4
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-07-03 16:14:45 +0900

    Update controller-sdk 1.1.0

--- sample/app/src/main/java/com/aromajoin/controllersdksample/MainActivity.java
+++ sample/app/src/main/java/com/aromajoin/controllersdksample/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import android.view.View;
-import android.widget.Button;
 import com.aromajoin.aromashootercontroller.connection.AromaShooterController;
 import com.aromajoin.aromashootercontroller.connection.model.AromaShooter;
 import com.aromajoin.aromashootercontroller.ui.activity.ASControllerBaseActivity;
@@ -75,30 +74,20 @@ public class MainActivity extends ASControllerBaseActivity implements View.OnCli
   /**
    * Uses runnable to send command which allows to diffuse multiple ports at the same time.
    */
-  List<AromaShooter> aromaShooters;
   private Runnable diffuseTask = new Runnable() {
     @Override public void run() {
       if (ports.size() == 0) {
         ports.clear();  // Clear buffered ports.
         return;
       }
-      aromaShooters = aromaShooterController.getConnectedDevices();
+      List<AromaShooter> aromaShooters = aromaShooterController.getConnectedDevices();
       if (aromaShooters == null || aromaShooters.size() == 0) { // check whether there is any connected devices.
         ports.clear();  // Clear buffered ports.
         return;
       }
       // Diffuse scents from selected ports of all connected devices.
-      aromaShooterController.diffuse(aromaShooters, DEFAULT_DURATION, 1, convertIntegers(ports));
+      aromaShooterController.diffuse(aromaShooters, DEFAULT_DURATION, true, ports);
       ports.clear();
     }
   };
-
-  // A helper method to convert List<Integer> to primitive integer array.
-  private int[] convertIntegers(List<Integer> integers) {
-    int[] ret = new int[integers.size()];
-    for (int i = 0; i < ret.length; i++) {
-      ret[i] = integers.get(i);
-    }
-    return ret;
-  }
 }
